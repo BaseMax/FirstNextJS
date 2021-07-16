27c6e086f80961e0c7e261a745bb3083f40dfea6
Max Base <MaxBaseCode@gmail.com>
2021-07-16 15:58:00 +0430

    remove node modules dir

--- node_modules/eslint-config-next/core-web-vitals.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['.'].map(require.resolve),
-  rules: {
-    '@next/next/no-sync-scripts': 2,
-    '@next/next/no-html-link-for-pages': 2,
-    '@next/next/no-img-element': 2,
-  },
-}
