a03f6f3094e92301416d5aa86bfb2704db7173d3
Max Base <MaxBaseCode@gmail.com>
2021-07-16 19:38:37 +0430

    Upload sample export build files

--- config/index.js
+++
@@ -1,5 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production'
-
-export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000';
-// https://maxbase.org
-
--- pages/api/articles/index.js
+++
@@ -1,5 +0,0 @@
-import { articles } from '../../../data'
-
-export default function handler(req, res) {
-  res.status(200).json(articles)
-}
