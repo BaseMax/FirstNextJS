76e741ad91b9e85982c77d21ee13c0c5c5134ee4
Max Base <MaxBaseCode@gmail.com>
2021-07-16 18:45:08 +0430

    adding single route for article

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const error = () => {
+
+    return (
+        <div>
+            <Head>
+                <title>404 Error page</title>
+            </Head>
+            <h1>404: Not Found!</h1>
+        </div>
+    )
+}
+export default error;
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,16 +1,41 @@
+import Link from 'next/link';
 import { useRouter } from "next/dist/client/router";
 
 const article = () => {
     const router = useRouter()
     const {id} = router.query
     return (
-        <div>
-            This is article {id}
-        </div>
+        <>
+            {/* <h1>This is article {id}</h1> */}
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <br />
+            <Link href='/'>Go back</Link>
+        </>
     )
 }
 
-export const getServerSideProps = async() => {
-    
-}
+// getServerSideProps
+export const getStaticProps = async(context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+
+    return {
+        props: {
+            article
+        }
+    }
+};
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const articles = await res.json()
+    const ids = articles.map(article => article.id)
+    const paths = ids.map(id => ({params:{id:id.toString()}}))
+    return {
+        paths,
+        fallback: false
+    }
+};
+
 export default article;
